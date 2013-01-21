805576b75802d57a8b2361b27de3419609de85ce
akorop <akorop@ukrcard.com.ua>
2013-01-21 16:41:23 +0200

    Working game Life

--- Domain/GameLife/ModelGameLife.cs
+++ Domain/GameLife/ModelGameLife.cs
@@ -4,20 +4,42 @@ using System.Linq;
 
 namespace Domain.GameLife
 {
+    /// <summary>
+    /// Class, that provides description of Game "Life"
+    /// </summary>
     public class ModelGameLife
     {
+        #region Variable
+        /// <summary>
+        /// Width of field
+        /// </summary>
         private int _widthField = 10;
+        /// <summary>
+        /// Height of field
+        /// </summary>
         private int _heightField = 10;
-        private readonly Cell[,] _arrayCell;
+        /// <summary>
+        /// Array of cells on field in game
+        /// </summary>
+        private Cell[,] _arrayCell;
+
+        #endregion
 
         #region Initialize
 
+        /// <summary>
+        /// Constructor without parameters of <see cref="ModelGameLife"/>
+        /// </summary>
         public ModelGameLife()
         {
             _arrayCell = new Cell[_widthField, _heightField];
             InitGameLifeEngine();
         }
-
+        /// <summary>
+        /// Constructor with parameters of <see cref="ModelGameLife"/>
+        /// </summary>
+        /// <param name="widthField">Width of field</param>
+        /// <param name="heightField">Height of field</param>
         public ModelGameLife(int widthField, int heightField)
         {
             _widthField = widthField;
@@ -26,11 +48,47 @@ namespace Domain.GameLife
             InitGameLifeEngine();
         }
 
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// Update next step of game
+        /// </summary>
         public void Update()
         {
             GameLifeEngineNextStep();
         }
-        
+        /// <summary>
+        /// Array of cells on field in game
+        /// </summary>
+        public Cell[,] Array
+        {
+            get { return _arrayCell; }
+        }
+        /// <summary>
+        /// Width of field
+        /// </summary>
+        public int WidthField
+        {
+            get { return _widthField; }
+            set { _widthField = value; }
+        }
+        /// <summary>
+        /// Height of field
+        /// </summary>
+        public int HeightField
+        {
+            get { return _heightField; }
+            set { _heightField = value; }
+        }
+
+        #endregion
+
+        #region Private
+        /// <summary>
+        /// Set begin status of game
+        /// </summary>
         private void InitGameLifeEngine()
         {
             var rand = new Random();
@@ -38,16 +96,21 @@ namespace Domain.GameLife
             {
                 for (int j = 0; j < _heightField; j++)
                 {
-                    _arrayCell[i,j] = new Cell(i,j);
+                    _arrayCell[i, j] = new Cell(i, j);
                     var t = rand.Next(2);
-                    if(t > 0.5)
-                        _arrayCell[i,j].SetOrganismStatus(OrganismStatus.Born);
+                    if (t < 1)
+                        _arrayCell[i, j].SetOrganismStatus(OrganismStatus.Born);
+
                 }
             }
         }
-
+        /// <summary>
+        /// Claculate next step of game
+        /// </summary>
         private void GameLifeEngineNextStep()
         {
+            var tempArray = new Cell[_widthField,_heightField];
+            InitArrayCell(ref tempArray);
             for (int i = 0; i < _widthField; i++)
             {
                 for (int j = 0; j < _heightField; j++)
@@ -55,26 +118,59 @@ namespace Domain.GameLife
                     var tempCountNeighborLiveOrganism = GetCountNeighborLiveOrganism(_arrayCell[i, j]);
                     if (tempCountNeighborLiveOrganism < 2 || tempCountNeighborLiveOrganism > 3)
                     {
-                        _arrayCell[i, j].SetOrganismStatus(OrganismStatus.Dead);
+                        if (_arrayCell[i, j].Status == OrganismStatus.Empty || _arrayCell[i, j].Status == OrganismStatus.Dead)
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Empty);
+                        }
+                        else
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Dead);
+                        }
                     }
                     else if (tempCountNeighborLiveOrganism == 3)
                     {
-                        _arrayCell[i, j].SetOrganismStatus(OrganismStatus.Born);
+                        if (_arrayCell[i, j].Status == OrganismStatus.Born ||
+                            _arrayCell[i, j].Status == OrganismStatus.Live)
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Born);
+                        }
+                        else
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Live);
+                        }
+                        
                     }
                     else
                     {
-                        _arrayCell[i, j].SetOrganismStatus(OrganismStatus.Live);
+                        if (_arrayCell[i, j].Status == OrganismStatus.Born ||
+                            _arrayCell[i, j].Status == OrganismStatus.Live)
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Live);
+                        }
+                        else
+                        {
+                            tempArray[i, j].SetOrganismStatus(OrganismStatus.Empty);
+                        }
                     }
                 }
             }
+            _arrayCell = tempArray;
         }
-
+        /// <summary>
+        /// Calculate live and born neighbor of cell
+        /// </summary>
+        /// <param name="cell">Current cell</param>
+        /// <returns>Count of neighbor</returns>
         private int GetCountNeighborLiveOrganism(Cell cell)
         {
             var tempNeighbor = GetNeighborCell(cell);
             return tempNeighbor.Count(item => item.Status == OrganismStatus.Born || item.Status == OrganismStatus.Live);
         }
-
+        /// <summary>
+        /// Create <see cref="IEnumerable{T}"/> of neighbor cell for current cell
+        /// </summary>
+        /// <param name="cell">Current cell</param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="Cell"/></returns>
         private IEnumerable<Cell> GetNeighborCell(Cell cell)
         {
             var list = new List<Cell>();
@@ -82,7 +178,7 @@ namespace Domain.GameLife
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (i != j)
+                    if (!(i == j && i == 1))
                     {
                         list.Add(GetCell(cell.X - 1 + i, cell.Y - 1 + j));
                     }
@@ -90,19 +186,24 @@ namespace Domain.GameLife
             }
             return list;
         }
-
+        /// <summary>
+        /// Return <see cref="Cell"/> for coordinats
+        /// </summary>
+        /// <param name="i">X</param>
+        /// <param name="j">Y</param>
+        /// <returns><see cref="Cell"/></returns>
         public Cell GetCell(int i, int j)
         {
-            var iTemp = 0;
-            var jTemp = 0;
+            var iTemp = i;
+            var jTemp = j;
 
             if (iTemp < 0)
             {
-                iTemp = _widthField - iTemp;
+                iTemp = _widthField + iTemp;
             }
             else if (iTemp > _widthField - 1)
             {
-                iTemp = (_widthField - 1)%iTemp;
+                iTemp = _widthField % iTemp;
             }
             else
             {
@@ -110,11 +211,11 @@ namespace Domain.GameLife
             }
             if (jTemp < 0)
             {
-                jTemp = _heightField - jTemp;
+                jTemp = _heightField + jTemp;
             }
             else if (jTemp > _heightField - 1)
             {
-                jTemp = (_heightField - 1) % jTemp;
+                jTemp = _heightField % jTemp;
             }
             else
             {
@@ -123,26 +224,19 @@ namespace Domain.GameLife
 
             return _arrayCell[iTemp, jTemp];
         }
-
-        #endregion
-
-        #region Public
-
-        public Cell[,] Array
+        /// <summary>
+        /// Create array of empty <see cref="Cell"/>
+        /// </summary>
+        /// <param name="array"></param>
+        private void InitArrayCell(ref Cell[,] array)
         {
-            get { return _arrayCell; }
-        }
-
-        public int WidthField
-        {
-            get { return _widthField; }
-            set { _widthField = value; }
-        }
-
-        public int HeightField
-        {
-            get { return _heightField; }
-            set { _heightField = value; }
+            for (int i = 0; i < _widthField; i++)
+            {
+                for (int j = 0; j < _heightField; j++)
+                {
+                    array[i,j] = new Cell(i,j);
+                }
+            }
         }
 
         #endregion
